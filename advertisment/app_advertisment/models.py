058859d90df6fecec250122a7db424a893e1cf5a
serlak <slakeev061@yandex.ru>
2023-08-03 18:05:23 +0300

    First commit

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length= 12 )
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='Отметьте, если торг уместен')
+    created_at= models.DateTimeField('Дата и время создания', auto_now_add=True)
+    updatet_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+
+
+
