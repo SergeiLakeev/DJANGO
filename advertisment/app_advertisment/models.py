893725e8116ae29aa45e45fdf96ecc617b27920a
serlak <slakeev061@yandex.ru>
2023-08-04 13:44:25 +0300

    ДЗ № 7

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -8,6 +8,11 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text='Отметьте, если торг уместен')
     created_at= models.DateTimeField('Дата и время создания', auto_now_add=True)
     updatet_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+    def __str__(self):
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+
+    class Meta:
+        db_table = 'advertisements'
 
 
 
